65f670095050ba59746e538cfe164e038b3f3999
Jaeuk Kwon <snust21itm@gmail.com>
2024-10-15 12:14:54 +0900

    Initial commit

---
+++ src/main/java/itm/oss/Main.java
@@ -0,0 +1,8 @@
+package itm.oss;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("The sum is " + calc.add(5, 5));
+    }
+}
